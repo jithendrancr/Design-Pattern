f9d4a2a2d488ccd3d46da3d32b70f27ac6a9d746
jithendrancr <117749076+jithendrancr@users.noreply.github.com>
2022-11-09 23:49:42 -0500

    Add files via upload

---
+++ Customer.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObserverPattern
+{
+    internal class Customer : ICustomer
+    {
+        public string CustomerName { get; set; }
+        public Customer(string customerName)
+        {
+            CustomerName = customerName;
+        }
+
+        public void Update(WeatherUpdates weather)
+        {
+            Console.WriteLine($"Weather Update for {CustomerName} is Temparature: {weather.Temparature.ToString()} And WindSpeed is : {weather.WindSpeed.ToString()}/KM at: {DateTime.Now.ToString()}");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using ObserverPattern;
+
+class Program
+    { 
+        static void Main(string[] args)
+        {
+        WeatherUpdates weatherUpdate = new WeatherUpdates(20.5f,72f);; 
+        Customer accuWeather = new Customer("AccuWeather");
+        Customer bbcWeather = new Customer("bbcWeather");
+        Customer skymet = new Customer("skymet");
+
+        weatherUpdate.AddCustomer(accuWeather);
+        weatherUpdate.AddCustomer(bbcWeather);
+        weatherUpdate.AddCustomer(skymet);
+
+        weatherUpdate.Temparature = 26.4f;
+        Console.WriteLine("---------------------------------------------------------------------");
+        weatherUpdate.WindSpeed = 81.5f;
+        Console.WriteLine("---------------------------------------------------------------------");
+
+        weatherUpdate.Temparature = 46.5f;
+        Console.WriteLine("---------------------------------------------------------------------");
+
+        weatherUpdate.WindSpeed = 80.8f;
+    }
+    }
